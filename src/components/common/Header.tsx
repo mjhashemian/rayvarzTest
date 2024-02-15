1ae977499baba3594b1718453b0c69ab86f05fdb
mhashemian <m.jhashemian81@gmail.com>
2024-02-15 16:56:35 +0330

    fix header Styles and add tailwind config

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,33 +1,24 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
-
+import Logo from '../../../public/icons/logo.svg';
 const pages = ['houses', 'about'];
 
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <AppBar style={{ background: '#FFF' }}>
+    <AppBar style={{ background: '#FFF', height: '68px' }}>
       <Container maxWidth="xl">
         <Toolbar>
-          <Typography
-            style={{
-              color: '#000',
-              border: '1px solid',
-              padding: '10px',
-              borderRadius: '10px',
-            }}
-          >
-            LOGO
-          </Typography>
+          <img src={Logo} className="w-16" alt="" />
+
           {pages.map((page) => (
             <Button
               onClick={() => navigate(`/${page}`)}
               key={page}
-              style={{ color: 'black', border: 'none' }}
+              style={{ color: 'black', outline: ' none ' }}
             >
               {page}
             </Button>
--- src/components/layout/Dashboard.tsx
+++ src/components/layout/Dashboard.tsx
@@ -4,10 +4,18 @@ import Header from '../common/Header';
 function DashboardLayout() {
   return (
     <div className="w-full min-h-[100vh] relative">
-      <div className="">
-        <Header/>
+      <div className="hidden md:hidden xl:block">{/* <SideBar /> */}</div>
+      <Header />
+      {/* <UserProfileDrawer open={openProfile} setOpen={setOpenProfile} /> */}
+      <div
+        style={{
+          minHeight: 280,
+          animation: 'ease-in-out',
+        }}
+        className=" w-full h-screen mt-[68px]"
+      >
+        <Outlet />
       </div>
-      <Outlet />
     </div>
   );
 }
